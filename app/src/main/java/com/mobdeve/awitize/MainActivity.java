99129850a21a044fc0bae254df81f18985d5d987
RJ-Csc <rolan_cosico@dlsu.edu.ph>
2021-08-17 01:43:09 +0800

    Added Home/Main, Library, and Music Player layouts

--- app/src/main/java/com/mobdeve/awitize/MainActivity.java
+++ app/src/main/java/com/mobdeve/awitize/MainActivity.java
@@ -3,12 +3,43 @@ package com.mobdeve.awitize;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_music_player);
+
+        /*
+        setContentView(R.layout.activity_main);
+        // FOR TESTING ONLY
+        Spinner spinner = findViewById(R.id.sp_category_select);
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("GENRE");
+        arrayList.add("ARTIST");
+        arrayList.add("ALBUM");
+        arrayList.add("PLAYLIST");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String tutorialsName = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + tutorialsName, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView <?> parent) {
+            }
+        });
+        */
     }
 }
